523627a04290343e399a7a5e291db393ec4f5b9d
Gabriel Abiak <89198274+Ab1ak@users.noreply.github.com>
2023-05-15 19:57:20 -0300

    finished the both files

--- common/teams_message_operator.py
+++ common/teams_message_operator.py
@@ -1,12 +1,16 @@
 from airflow.operators.python_operator import PythonOperator
 from airflow.models import Connection
 from airflow.models import TaskInstance
+import datetime
 import urllib3
+import json
 import typing
+import pytz
+
 
 class TeamsMessageOperator(PythonOperator):
     
-    def __init__(self, task_id: str, hookurl: str,
+    def _init_(self, task_id: str, hookurl: str,
                 executor_config: typing.Dict, payload: typing.Dict={}, 
                 http_timeout: int=60, **context):
         self.hookurl = hookurl
@@ -14,12 +18,44 @@ class TeamsMessageOperator(PythonOperator):
         self.http = urllib3.PoolManager()
         self.http_timeout = http_timeout
 
-        super().__init__(
+        super()._init_(
             task_id = task_id,
             executor_config = executor_config,
             python_callable=self.send_message_to_teams, 
             provide_context=True,
-            trigger_rule='all_done')   
+            trigger_rule='all_done')       
+
+    def text(self, mtext: str):     
+            self.payload["type"] = "message"
+            self.payload["attachments"] = [{
+                "contentType": "application/vnd.microsoft.card.adaptive",
+                "contentUrl": None,
+                "content": {
+                    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                    "type": "AdaptiveCard",
+                    "version": "1.4",
+                    "body": [{"type": "TextBlock", "wrap": True, "text": mtext}],
+                    "msteams": {"width": "Full"}
+            }
+        }]
+
+    def send(self):
+        headers = {"Content-Type":"application/json"}
+        r = self.http.request(
+                'POST',
+                f'{self.hookurl}',
+                body=json.dumps(self.payload).encode('utf-8'),
+                headers=headers, timeout=self.http_timeout)
+        if r.status == 200: 
+            return True
+        else:
+            raise Exception(r.reason)
+
+    def format_date(self, execution_date):
+        utc_data = datetime.datetime.fromisoformat(execution_date)# Convertendo a string para um objeto datetime
+        local_date = utc_data.astimezone(pytz.timezone('America/Sao_Paulo'))# Aplicando o fuso horário na data e hora
+        formatted_date = local_date.strftime('%d-%m-%Y %H:%M')# Formatando a data e hora
+        return formatted_date
 
     def send_message_to_teams(self, **context):
         formatted_date = self.format_date(str(context['execution_date']))
@@ -29,20 +65,29 @@ class TeamsMessageOperator(PythonOperator):
 
         for task in dag_instance.tasks:
             task_status = TaskInstance(task, context['execution_date']).current_state()
-            if task_status not in ['success', 'running', 'skipped']:
+            if task_status not in ['success', 'running']:
                 lista_task.append(task.task_id)
+        print(len(lista_task))
         if len(lista_task) > 0:
+            final_message = f"""
+                        
+                        *DAG:* {(dag_id)}\n
 
-            final_message = f"""                        
-                        **DAG:** {(dag_id)}\n
-                        **Tasks com falha:** {(lista_task)}\n
-                        **Execution Time:** {(formatted_date)}\n
+                        *Tasks com falha:* {(lista_task)}\n
+
+                        *Execution Time:* {(formatted_date)}\n
+
                         """           
-            print(final_message)
+            self.text(mtext= final_message)
         else:
             final_message = f"""
-                        **DAG:** {(dag_id)}\n
-                        **Nenhuma Task falhou!**\n
-                        **Execution Time:** {(formatted_date)}\n
+                        *DAG:* {(dag_id)}\n
+
+                        *Nenhuma Task falhou!*\n
+
+                        *Execution Time:* {(formatted_date)}\n
+
                         """
-            print(final_message)
+            self.text(mtext= final_message)
+            
+        self.send()
--- webhook.py
+++ webhook.py
@@ -4,52 +4,63 @@ from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.python_operator import PythonOperator
 
-sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0,os.path.abspath(os.path.dirname(_file_)))
 
 from common.teams_message_operator import TeamsMessageOperator
 
 DAG_ID = 'Webhook'
 
-def throw_exception():
-    statuss = 1
-    if statuss == 1:
-        print("Tudo certo, pode continuar")
-    else:
-        raise Exception('deu ruim')
+EXECUTOR_CONFIG= {
+    'KubernetesExecutor' : { 
+        'request_cpu' : '2',
+        'request_memory' : '2Gi',
+        'namespace' : " "
+    }
+}
 
 default_args = {
     'owner': 'Webhook',
     'depends_on_past': False,
-    "start_date": datetime(2023, 4, 15),
+    "start_date": datetime(2023, 3, 7),
     "retries": 0,
     "retry_delay": timedelta(seconds=5),
     'execution_timeout': timedelta(hours=2)
 }
 
+def throw_exception():
+    statuss = 2
+    if statuss == 1:
+        print("Tudo certo, pode continuar")
+    else:
+        raise Exception('deu ruim')
+
 with DAG(
     dag_id = DAG_ID,
     default_args = default_args,
     max_active_runs = 1,
-    schedule_interval= None, #agendamento
-    catchup = False, #recuperadas dags anteriores
-    doc_md = "DAG para teste de operador de webhook",
-    tags=["Webhook"]
+    schedule_interval= None,
+    catchup = False,
+    doc_md = "DAG somente para teste de operador de webhook",
+    tags=["teams", "Webhook"]
     ) as dag:
 
     start_task = PythonOperator(
         task_id = 'start_task',
-        python_callable = throw_exception
+        python_callable = throw_exception,
+        executor_config=EXECUTOR_CONFIG
         )
 
     teste_task = PythonOperator(
         task_id = 'teste_task',
-        python_callable = throw_exception
+        python_callable = throw_exception,
+        executor_config=EXECUTOR_CONFIG
         )
 
     send_teams_message = TeamsMessageOperator(
         task_id = 'send_teams_message',
-        mention_on_fail = ["CS383950"],
-        hookurl = 'link do hook do teams'
+        hookurl = 'adicionar aqui o link do webhook', #adicionar aqui o link do webhook
+        executor_config=EXECUTOR_CONFIG
         )
-    
-    start_task >> teste_task >> send_teams_message 
+
+
+    start_task >> teste_task >> send_teams_message
